84e9b5c7d241bd30c66d9bcab6b049e9629a4b98
Jjcofa <115888631+Jjcofa@users.noreply.github.com>
2023-10-17 07:53:57 +0200

    camelCase

--- dz12/script.js
+++ dz12/script.js
@@ -1,15 +1,15 @@
 // Первое
 
-let Input1 = prompt("Первая строка:");
-let Input2 = prompt("Вторая строка:");
-let Input3 = prompt("Третья строка:");
-let concatenate = Input1 + " " + Input2 + " " + Input3;
+let userInput1 = prompt("Первая строка:");
+let userInput2 = prompt("Вторая строка:");
+let userInput3 = prompt("Третья строка:");
+let concatenate = userInput1 + " " + userInput2 + " " + userInput3;
 
 console.log("Результат: " + concatenate);
 
 // Второе 
 
-let Number = prompt("Введите пятизначное число:");
-let split = Number.split("");
+let userNumber = prompt("Введите пятизначное число:");
+let split = userNumber.split("");
 
 console.log("Разбитые цифры: " + split.join(" "));
