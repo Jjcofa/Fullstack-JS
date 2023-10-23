a18fad8d9730d0212e5a76798eb4fb6ea7b8e453
Jjcofa <115888631+Jjcofa@users.noreply.github.com>
2023-10-23 11:21:26 +0200

    hw14

---
+++ dz14/script.js
@@ -0,0 +1,5 @@
+let userNumber1 = +prompt("Перше число:");
+let userNumber2 = +prompt("Друге число:");
+let userNumber3 = +prompt("Третє число:");
+
+alert(`Середнє: ${(userNumber1 + userNumber2 + userNumber3)/3}`);
